9b1ef7b5c47f7597e59a1969d192177ae282b805
Salma Ben Brahim <benbrahimsalma18@gmail.com>
2025-03-25 21:06:50 +0100

    commit changes of src/temp.js

--- src/temp.js
+++ src/temp.js
@@ -2,7 +2,7 @@
 const temp = 'temp';
 const temp2 = 'testing the testing branch';
 const temp3 = 'adding this to master branch';
-const tem4 = 'testiong the version control suppert of vs code';
+const tem4 = 'testing the version control support of vs code';
 
 
 
